01a52f282e397659294d83fb81a7a114a7301aed
Luan Machado <94332377+luanmachadoyt@users.noreply.github.com>
2023-03-12 19:06:05 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -23,3 +23,103 @@ function toggleMenu(){
 
 btnMobile.addEventListener('click', toggleMenu);
 
+
+
+//-------------------------------CSS---------------------------------------------------//
+
+var icones = document.querySelector(".css3")
+
+icones.addEventListener("mouseover", function() {
+
+    var legenda = document.querySelector("#text-css")
+
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.remove("hide")
+    texto_padrao.classList.add("hide")
+
+})
+
+icones.addEventListener("mouseout", function() {
+
+    var legenda = document.querySelector("#text-css")
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.add("hide")
+    texto_padrao.classList.remove("hide")
+
+})
+
+//--------------------------------HTML-----------------------------------------------------//
+
+var icones = document.querySelector(".html5")
+
+icones.addEventListener("mouseover", function() {
+
+    var legenda = document.querySelector("#text-html")
+
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.remove("hide")
+    texto_padrao.classList.add("hide")
+
+})
+
+icones.addEventListener("mouseout", function() {
+
+    var legenda = document.querySelector("#text-html")
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.add("hide")
+    texto_padrao.classList.remove("hide")
+
+})
+
+//--------------------------------JS-----------------------------------------------------//
+
+var icones = document.querySelector(".js")
+
+icones.addEventListener("mouseover", function() {
+
+    var legenda = document.querySelector("#text-js")
+
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.remove("hide")
+    texto_padrao.classList.add("hide")
+
+})
+
+icones.addEventListener("mouseout", function() {
+
+    var legenda = document.querySelector("#text-js")
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.add("hide")
+    texto_padrao.classList.remove("hide")
+
+})
+//--------------------------------FIGMA-----------------------------------------------------//
+
+var icones = document.querySelector(".figma")
+
+icones.addEventListener("mouseover", function() {
+
+    var legenda = document.querySelector("#text-figma")
+
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.remove("hide")
+    texto_padrao.classList.add("hide")
+
+})
+
+icones.addEventListener("mouseout", function() {
+
+    var legenda = document.querySelector("#text-figma")
+    var texto_padrao = document.querySelector("#text-padrao")
+
+    legenda.classList.add("hide")
+    texto_padrao.classList.remove("hide")
+
+})
